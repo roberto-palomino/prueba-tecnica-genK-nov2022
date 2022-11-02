69beb9d9f20f963ff79e3e84f41d5059e5ecdc9c
Bihotxi <roberpdlcruz@hotmail.com>
2022-11-02 17:44:24 +0100

    fix:add function to duplicate the element button

---
+++ ejercicio1c.js
@@ -0,0 +1,15 @@
+function duplicateButtoms(n) {
+    
+  const createButton = () => {
+    for (let i = 1; i <= n; i++) {
+      const body = document.querySelector("body");
+      const button = document.createElement("button");
+      button.innerText = `${i} botón`;
+
+      body.appendChild(button);
+    }
+  };
+  createButton();
+}
+
+duplicateButtoms(10);
