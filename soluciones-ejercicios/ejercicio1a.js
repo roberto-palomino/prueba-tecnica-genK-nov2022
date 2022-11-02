acea2c17bd4e5a5b8fc957fe50d266d9f5652ff8
Bihotxi <roberpdlcruz@hotmail.com>
2022-11-02 14:24:31 +0100

    feat:add function to create a button

---
+++ soluciones-ejercicios/ejercicio1a.js
@@ -0,0 +1,8 @@
+function createButton() {
+const body = document.querySelector('body');
+const button = document.createElement('button');
+button.innerText = '1 botón';
+
+body.appendChild(button);
+}
+createButton()
