2b6f15986e7e59efc6c10e08039965ca42d1d448
Bihotxi <roberpdlcruz@hotmail.com>
2022-11-08 10:58:02 +0100

    feat:add code with Kairos solution

--- ejercicio1e.js
+++ ejercicio1e.js
@@ -1,22 +1,43 @@
-function duplicateButtomswithHoverEffect(n) {
-  for (let i = 1; i <= n; i++) {
-    createButtonWithHoverEffect(i);
-  }
+// function duplicateButtomswithHoverEffect(n) {
+//   for (let i = 1; i <= n; i++) {
+//     createButtonWithHoverEffect(i);
+//   }
+// }
+
+// const createButtonWithHoverEffect = (i) => {
+//   const defaultText = `${i} botón`;
+//   const body = document.querySelector("body");
+//   const button = document.createElement("button");
+//   button.innerText = defaultText;
+//   button.onmouseover = () => {
+//     button.innerText = `${i} botóN`;
+//   };
+//   button.onmouseout = () => {
+//     button.innerText = defaultText;
+//   };
+
+//   body.appendChild(button);
+// };
+
+// duplicateButtomswithHoverEffect(1000);
+
+// Solución de Kairos:
+
+for (let contadorBotones = 1; contadorBotones <= 1000; contadorBotones += 1) {
+  const boton = document.createElement("button");
+  boton.textContent = `${contadorBotones} botón`;
+  document.body.appendChild(boton);
 }
 
-const createButtonWithHoverEffect = (i) => {
-  const defaultText = `${i} botón`;
-  const body = document.querySelector("body");
-  const button = document.createElement("button");
-  button.innerText = defaultText;
-  button.onmouseover = () => {
-    button.innerText = `${i} botóN`;
-  };
-  button.onmouseout = () => {
-    button.innerText = defaultText;
-  };
-
-  body.appendChild(button);
-};
-
-duplicateButtomswithHoverEffect(1000);
+document.body.addEventListener("mouseover", (e) => {
+  const boton = e.target;
+  if (boton.tagName === "BUTTON") {
+    boton.textContent = boton.textContent.slice(0, -1) + "N";
+  }
+});
+document.body.addEventListener("mouseout", (e) => {
+  const boton = e.target;
+  if (boton.tagName === "BUTTON") {
+    boton.textContent = boton.textContent.slice(0, -1) + "n";
+  }
+});
