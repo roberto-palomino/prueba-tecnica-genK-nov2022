13c796f9eae5969f4b52c5bb3001cc9b4c3796bd
Bihotxi <roberpdlcruz@hotmail.com>
2022-11-08 10:47:01 +0100

    feat:add code with Kairos solution

--- ejercicio1b.js
+++ ejercicio1b.js
@@ -14,3 +14,16 @@ function createButtonWithHoverEffect() {
   };
 }
 createButtonWithHoverEffect();
+
+// Solución de Kairos:
+
+const boton = document.createElement("button");
+boton.textContent = "1 botón";
+document.body.appendChild(boton);
+
+boton.addEventListener("mouseover", () => {
+  boton.textContent = boton.textContent.slice(0, -1) + "N";
+});
+boton.addEventListener("mouseout", () => {
+  boton.textContent = boton.textContent.slice(0, -1) + "n";
+});
