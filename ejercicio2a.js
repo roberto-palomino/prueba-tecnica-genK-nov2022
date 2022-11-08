b6a0b97e3e885c8e395a8e5aa7f6e30eb5c9019e
Bihotxi <roberpdlcruz@hotmail.com>
2022-11-08 11:11:55 +0100

    feat:add code with Kairos solution

--- ejercicio2a.js
+++ ejercicio2a.js
@@ -10,52 +10,67 @@ const array2 = [
   [6, 4, 2],
 ];
 
-const result = [];
-const array2Columns = [];
-const flatArray1Rows = [];
-const flatArray2Columns = [];
+// const result = [];
+// const array2Columns = [];
+// const flatArray1Rows = [];
+// const flatArray2Columns = [];
 
-function clasifyArrayColumns(arr) {
-  const column1Array2 = [];
-  const column2Array2 = [];
-  const column3Array2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    const row = arr[i];
+// function clasifyArrayColumns(arr) {
+//   const column1Array2 = [];
+//   const column2Array2 = [];
+//   const column3Array2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     const row = arr[i];
 
-    for (let j = 0; j < row.length; j++) {
-      const columnValue = row[j];
-      if (j === 0) {
-        column1Array2.push(columnValue);
-      }
-      if (j === 1) {
-        column2Array2.push(columnValue);
-      }
-      if (j === 2) {
-        column3Array2.push(columnValue);
-      }
-    }
+//     for (let j = 0; j < row.length; j++) {
+//       const columnValue = row[j];
+//       if (j === 0) {
+//         column1Array2.push(columnValue);
+//       }
+//       if (j === 1) {
+//         column2Array2.push(columnValue);
+//       }
+//       if (j === 2) {
+//         column3Array2.push(columnValue);
+//       }
+//     }
+//   }
+//   array2Columns.push(column1Array2, column2Array2, column3Array2);
+// }
+
+// function flatArray(arr, finalArray) {
+//   for (const element of arr) {
+//     for (const value of element) {
+//       finalArray.push(value);
+//     }
+//   }
+// }
+
+// function multiplyArrayValues(a, b) {
+//   for (let i = 0; i < Math.max(a.length, b.length); i++) {
+//     result.push(a[i] * b[i]);
+//   }
+//   return result;
+// }
+
+// clasifyArrayColumns(array2);
+// flatArray(array1, flatArray1Rows);
+// flatArray(array2Columns, flatArray2Columns);
+// multiplyArrayValues(flatArray1Rows, flatArray2Columns);
+
+// console.log("resultado:", result);
+
+// Solución de Kairos:
+
+const result = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+for (let rowArr1 = 0; rowArr1 < array1.length; rowArr1 += 1) {
+  for (let colArr1 = 0; colArr1 < array1[rowArr1].length; colArr1 += 1) {
+    result[rowArr1][colArr1] =
+      array1[rowArr1][colArr1] * array2[colArr1][rowArr1];
   }
-  array2Columns.push(column1Array2, column2Array2, column3Array2);
 }
-
-function flatArray(arr, finalArray) {
-  for (const element of arr) {
-    for (const value of element) {
-      finalArray.push(value);
-    }
-  }
-}
-
-function multiplyArrayValues(a, b) {
-  for (let i = 0; i < Math.max(a.length, b.length); i++) {
-    result.push(a[i] * b[i]);
-  }
-  return result;
-}
-
-clasifyArrayColumns(array2);
-flatArray(array1, flatArray1Rows);
-flatArray(array2Columns, flatArray2Columns);
-multiplyArrayValues(flatArray1Rows, flatArray2Columns);
-
-console.log("resultado:", result);
